6686ea748eba16250c944d2a6e7ebf4e376286fa
David Herrera <jodaniheso@gmail.com>
2021-07-27 11:42:27 -0600

    Processing formuls of which geometry figure with JS and HTML

--- figuras.js
+++ figuras.js
@@ -5,11 +5,8 @@
 //   return alert(`Su cuadrado tiene ${lado} unidades de perímetro`)
 // }
 // pericuad()
-const PI = 3.1415
-function pericuad (lado) {
-  let oper = lado * 4
-  return console.log(`Su cuadrado tiene ${oper} unidades de perímetro`)
-}
+const PI = 3.1416
+perimetroCuadrado = (lado) => lado * 4
 
 //Área del cuadrado
 // function areacuad () {
@@ -18,27 +15,67 @@ function pericuad (lado) {
 //   return alert(`Su cuadrado tiene ${lado} unidades de área`)
 // }
 // areacuad()
-function areacuad (lado) {
-  let oper = Math.pow(lado, 2)
-  return console.log(`Su cuadrado tiene ${oper} unidades de área`)
+areaCuadrado = (lado) => Math.pow(lado, 2)
+
+perimetroTriangulo = (a, b, c) => a + b + c 
+
+areaTriangulo = (base, altura) => (base * altura) / 2
+
+perimetroCirculo = (radio) =>  2 * PI * radio
+
+areaCirculo = (radio) => PI * Math.pow(radio, 2)
+
+// Interacción con el HTML
+function calcularAreaCuadrado() {
+  let input = document.getElementById("inputCuadrado")
+  let value = input.value
+
+  let area = areaCuadrado(value)
+  alert(`Su cuadrado tiene ${area} unidades de área.`)
 }
 
-function peritri (a, b, c) {
-  let sum = a + b + c
-  return console.log(`Su tríangulo tiene ${sum} unidades de perímetro`)
+function calcularPerimetroCuadrado() {
+  let input = document.getElementById("inputCuadrado")
+  let value = input.value
+
+  let perimetro = perimetroCuadrado(value)
+  alert(`Su cuadrado tiene ${perimetro} unidades de perímetro.`)
 }
 
-function areatri (base, altura) {
-  let area = (base * altura) / 2
-  return console.log(`Su tríangulo tiene ${area} unidades de área`)
+function calcularPerimetroTriangulo() {
+  let inputA = document.getElementById("inputTrianguloA")
+  let valueA = parseFloat(inputA.value)
+  let inputB = document.getElementById("inputTrianguloB")
+  let valueB = parseFloat(inputB.value)
+  let inputC = document.getElementById("inputTrianguloC")
+  let valueC = parseFloat(inputC.value)
+
+  let perimetro = perimetroTriangulo(valueA, valueB, valueC)
+  alert(`Su tríangulo tiene ${perimetro} unidades de perímetro.`)
 }
 
-function pericir (diametro) {
-  let perimetro = PI * diametro
-  return console.log(`Su circunferencia mide ${perimetro} unidades de perímetro`)
+function calcularAreaTriangulo() {
+  let inputB = document.getElementById("inputTrianguloBase")
+  let valueB = parseFloat(inputB.value)
+  let inputH = document.getElementById("inputTrianguloH")
+  let valueH = parseFloat(inputH.value)
+
+  let area = areaTriangulo(valueB, valueH)
+  alert(`Su tríangulo tiene ${area} unidades de área.`)
 }
 
-function areacir (radio) {
-  let area = PI * Math.pow(radio, 2)
-  return console.log(`Su circunferencia mide ${area} unidades de área`)
+function calcularPerimetroCirculo() {
+  let inputRadio = document.getElementById("inputCirculoRadio")
+  let valueRadio = parseFloat(inputRadio.value)
+
+  let perimetro = perimetroCirculo(valueRadio)
+  alert(`Su circunferencia mide ${perimetro.toFixed(2)} unidades de perímetro.`)
+}
+
+function calcularAreaCirculo () {
+  let inputRadio = document.getElementById("inputCirculoRadio")
+  let valueRadio = parseFloat(inputRadio.value)
+
+  let area = areaCirculo(valueRadio)
+  alert(`Su circunferencia mide ${area.toFixed(2)} unidades de perímetro.`)
 }
