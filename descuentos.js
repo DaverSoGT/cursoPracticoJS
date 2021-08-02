fa63cb8249f9a395d631eed8872941164351c237
David Herrera <jodaniheso@gmail.com>
2021-08-02 11:24:09 -0600

    Change the modality from the program of discounts to coupons

--- descuentos.js
+++ descuentos.js
@@ -3,10 +3,35 @@ descuentoCalculo = (precio, descuento) => (precio*(100 - descuento))/100
 function calcularDescuento() {
   let precioSinDescuento = document.getElementById("input-price")
   let valuePrecio = parseFloat(precioSinDescuento.value)
-  let inputDescuento = document.getElementById("input-discount")
-  let valueDescuento = parseFloat(inputDescuento.value)
-  let textResult = document.getElementById("showPrice")
+  let inputCoupon = document.getElementById("input-coupon")
+  let valueCoupon = inputCoupon.value
 
-  let precioConDescuento = descuentoCalculo(valuePrecio, valueDescuento)
-  textResult.innerText = `El precio de su producto es: $${precioConDescuento}.`
+  let coupons = [
+    {
+      name: 'Enorme David',
+      discount: 50
+    },
+    {
+      name: 'Fin de año',
+      discount: 30
+    },
+    {
+      name: 'Feliz año nuevo',
+      discount: 15
+    }
+  ]
+
+  let isCouponValueValid = (coupons) => coupons.name === valueCoupon
+
+  let userCoupon = coupons.find(isCouponValueValid)
+
+  if (!userCoupon) {
+    alert(`El cupón ${valueCoupon} no es valido.`)
+  } else {
+    let descuento = userCoupon.discount
+    let precioConDescuento = descuentoCalculo(valuePrecio, descuento)
+
+    let resultP = document.getElementById("showPrice")
+    resultP.innerText = `El precio de su producto es: $${precioConDescuento}`
+  }
 }
